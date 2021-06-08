3e0ec996bb4983edef7d9e8a401cbdefe09ed80f
hihello1123 <hihello1123@naver.com>
2021-06-08 20:03:37 +0900

    Fix app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+require("dotenv").config();
 
 function App() {
+  const [hello, setHello] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchHello = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}`);
+      setHello(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchHello();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">Hello, World!</header>
+      {loading ? (
+        <header className="App-header">로딩중...</header>
+      ) : (
+        <header className="App-header">{hello}</header>
+      )}
     </div>
   );
 }
