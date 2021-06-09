4172df7c4f7719942c5f9ab73ebb7830d72c0e4d
solheee <09merus@gmail.com>
2021-06-09 20:17:31 +0900

    Feat Router setting

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,16 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-function Nav() {
+function Nav({ isLoggedIn }) {
   return (
     <div className="nav">
       <div className="container">
-        <img
-          src="https://cdn.discordapp.com/attachments/851263973742739471/852072037240406036/BBira.png"
-          alt="BBira"
-        />
+        <Link to="/">
+          <img
+            src="https://cdn.discordapp.com/attachments/851263973742739471/852072037240406036/BBira.png"
+            alt="BBira"
+          />
+        </Link>
 
-        {/* 상태에 따라 변하는 오른쪽 버튼들 */}
-        <button className="mediumBtn reverse">Get start</button>
+        <div className="dynamicLink">
+          {
+            /* 상태에 따라 변하는 오른쪽 버튼들
+            1. 로그인 상태가 아닐 때 - 겟 스타트
+            2. 로그인일 중 일때 - 마이페이지, 로그아웃
+        */
+            isLoggedIn ? (
+              <>{/* 마이페이지, 로그아웃 버튼 */}</>
+            ) : (
+              <Link to="/login">
+                <button className="mediumBtn reverse">Get start</button>
+              </Link>
+            )
+          }
+        </div>
       </div>
     </div>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,79 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+import axios from 'axios';
 
-function Login() {
+function Login({ isLogin }) {
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+  });
+  const inputHandler = (e) => {
+    e.target.classList.remove('err');
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+    console.log(inputs);
+  };
+  const loginRequestHandler = async (e) => {
+    if (!inputs.email || !inputs.password) {
+      if (!inputs.email) {
+        e.target.form[0].classList.add('err');
+      }
+      if (!inputs.password) {
+        e.target.form[1].classList.add('err');
+      }
+      return;
+    }
+    e.preventDefault();
+
+    //axios
+    await axios
+      .post(
+        `${process.env.REACT_APP_API_URL}/login`,
+        {
+          email: inputs.email,
+          password: inputs.password,
+        },
+        {
+          'Content-Type': 'application/json',
+          withCredentials: true,
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        // 로그인 핸들러
+        isLogin(true);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="login container">
       <h1>로그인</h1>
 
       <form className="loginForm">
         <div className="inputGuoup">
-          <label for="email">email</label>
-          <input name="email" required></input>
+          <label htmlFor="email">email</label>
+          <input
+            name="email"
+            type="email"
+            required
+            onChange={inputHandler}
+          ></input>
         </div>
 
         <div className="inputGuoup">
-          <label for="password">password</label>
-          <input name="password" required></input>
+          <label htmlFor="password">password</label>
+          <input
+            name="password"
+            type="password"
+            required
+            onChange={inputHandler}
+          ></input>
         </div>
 
-        <button className="mediumBtn singIn">Sign in</button>
+        <button className="mediumBtn singIn" onClick={loginRequestHandler}>
+          Sign in
+        </button>
         <button className="mediumBtn reverse singUp">Sign up</button>
       </form>
     </div>
