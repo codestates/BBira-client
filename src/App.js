b3019fddcda310e01edb3d838dcfcc8595bfedfb
solheee <09merus@gmail.com>
2021-06-09 17:53:42 +0900

    Feat Login.js, stylesheet

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import axios from "axios";
-require("dotenv").config();
+import React, { useState, useEffect } from 'react';
+import './Style.css';
+import Nav from './component/Nav';
+import Login from './pages/Login';
+import axios from 'axios';
+require('dotenv').config();
 
 function App() {
-  const [hello, setHello] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  const fetchHello = async () => {
-    try {
-      setLoading(true);
-      const response = await axios.get(`${process.env.REACT_APP_API_URL}`);
-      setHello(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchHello();
-  }, []);
-
   return (
     <div className="App">
-      {loading ? (
-        <header className="App-header">로딩중...</header>
-      ) : (
-        <header className="App-header">{hello}</header>
-      )}
+      <Nav />
+      <Login />
     </div>
   );
 }
