99714492e4faaf0ef99d6449fed607acd96aef33
hihello1123 <hihello1123@naver.com>
2021-06-15 18:23:00 +0900

    Fix itemlist

--- src/component/Item.js
+++ src/component/Item.js
@@ -2,10 +2,33 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWonSign } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import { Route } from 'react-router';
 
-function Item({ item, isBtn }) {
-  const { itemname, itemprice, itemdesc, itemphoto } = item;
+function Item({ item, isBtn, isLoggedIn, clickedBtn }) {
+  const { itemname, itemprice, itemdesc, itemphoto, id } = item;
+  const history = useHistory();
 
+  const deleteItem = () => {
+    axios
+      .post(
+        `${process.env.REACT_APP_API_URL}/dropitem`,
+        { id },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${isLoggedIn.accessToken}`,
+          },
+          withCredentials: true,
+        }
+      )
+      .then(() => {
+        history.push({ pathname: '/mypage' });
+      })
+      .then(() => {
+        history.push({ pathname: '/mystore' });
+      });
+  };
   return (
     <div className="itemSlot">
       <div className="itemphotoContainer">
@@ -20,8 +43,12 @@ function Item({ item, isBtn }) {
 
       {isBtn ? (
         <div className="buttonContainer">
-          <button className="smallBtn itemEditBtn">Edit</button>
-          <button className="smallBtn itemDeleteBtn red">Delete</button>
+          <button className="smallBtn itemEditBtn" id={id} onClick={clickedBtn}>
+            Edit
+          </button>
+          <button className="smallBtn itemDeleteBtn red" onClick={deleteItem}>
+            Delete
+          </button>
         </div>
       ) : (
         <></>
